3f085898c293d355de94c39f855fbfdcf09ec0e6
GUILHERME.BRISIDA <guilherme.brisida@lince.com.br>
2024-06-14 09:36:32 -0300

    Initial commit

---
+++ src/main/java/br/com/lince/hackathon/HelloServlet.java
@@ -0,0 +1,36 @@
+package br.com.lince.hackathon;
+
+import com.github.jknack.handlebars.Handlebars;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    final Logger logger = Logger.getLogger(HelloServlet.class.getName());
+
+    @Override
+    public void init() {
+        logger.info("[helloServlet] servlet inicializado");
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var handlebars = new Handlebars();
+        final var template = handlebars.compile("templates/sample");
+        final var params = new HashMap<String, Object>(Map.of("message", "Olá mundo"));
+        final var html = template.apply(params);
+
+        response.setContentType("text/html");
+        response.getWriter().write(html);
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("[helloServlet] servlet finalizado");
+    }
+}
