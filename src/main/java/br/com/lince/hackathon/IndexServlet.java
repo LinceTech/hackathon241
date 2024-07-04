38741eabf306d8c10561f505e921058b25d22374
GUILHERME.BRISIDA <guilherme.brisida@lince.com.br>
2024-07-03 22:06:40 -0300

    exemplo

--- src/main/java/br/com/lince/hackathon/IndexServlet.java
+++ src/main/java/br/com/lince/hackathon/IndexServlet.java
@@ -1,6 +1,6 @@
 package br.com.lince.hackathon;
 
-import com.github.jknack.handlebars.Handlebars;
+import br.com.lince.hackathon.standard.TemplateRenderer;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,25 +16,10 @@ public class IndexServlet extends HttpServlet {
     private final Logger logger = Logger.getLogger(IndexServlet.class.getName());
 
     @Override
-    public void init() {
-        logger.info("[indexServlet] servlet inicializado");
-    }
-
-    @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        logger.info("Responder requisição [olá mundo]");
-
-        final var handlebars = new Handlebars();
-        final var template = handlebars.compile("templates/index");
+        final var renderer = new TemplateRenderer<>("index", response);
         final var params = new HashMap<String, Object>(Map.of("message", "Olá mundo..."));
-        final var html = template.apply(params);
 
-        response.setContentType("text/html");
-        response.getWriter().write(html);
-    }
-
-    @Override
-    public void destroy() {
-        logger.info("[indexServlet] servlet finalizado");
+        renderer.render(params);
     }
 }
