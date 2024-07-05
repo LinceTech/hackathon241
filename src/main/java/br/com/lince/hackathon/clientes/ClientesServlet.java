1089b9133890a38eaf7c981dfe7248c1ec1b65a4
unknown <Administrador@LIN176.NT-LINCE.SERVER>
2024-07-05 14:25:03 -0300

    ...

--- src/main/java/br/com/lince/hackathon/clientes/ClientesServlet.java
+++ src/main/java/br/com/lince/hackathon/clientes/ClientesServlet.java
@@ -1,6 +1,11 @@
 package br.com.lince.hackathon.clientes;
 
+import br.com.lince.hackathon.foo.Foo;
+import br.com.lince.hackathon.foo.FooRepository;
+import br.com.lince.hackathon.foo.FooViewData;
+import br.com.lince.hackathon.standard.JDBIConnection;
 import br.com.lince.hackathon.standard.TemplateRenderer;
+import com.github.jknack.handlebars.internal.lang3.math.NumberUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
 @WebServlet("/clientes/*")
@@ -26,6 +33,8 @@ public class ClientesServlet extends HttpServlet {
             case "/new":
                 carregaClientesModal(response);
                 break;
+            case "/save":
+                insereCliente(request, response);
             default:
                 response.getWriter().write("Not found : " + requestPath);
         }
@@ -46,5 +55,56 @@ public class ClientesServlet extends HttpServlet {
         final var renderer = new TemplateRenderer<Clientes>("/clientes/ClientePage", response);
 
         renderer.render(new Clientes());
+    }
+
+    void insereCliente(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var renderer = new TemplateRenderer<FooViewData>("clientes/ClientePage", response);
+        final var page = NumberUtils.toInt(request.getParameter("page"), 0);
+        final var bar = NumberUtils.toInt(request.getParameter("bar"), 0);
+        final var nome = request.getParameter("nome");
+        final var nome = request.getParameter("nome");
+        final var nome = request.getParameter("nome");
+        final var nome = request.getParameter("nome");
+        final var nome = request.getParameter("nome");
+        final var nome = request.getParameter("nome");
+        final var nome = request.getParameter("nome");
+        final var boo = request.getParameter("boo");
+        final var clientes = new Clientes(nome);
+        final var errors = new HashMap<String, String>();
+
+        if (nome.isBlank()) {
+            errors.put("basError", "Não pode ser vazio");
+        } else if (nome.length() > 60) {
+            errors.put("basError", "Não pode ser maior que 60 caracteres");
+        }
+
+        if (boo.isBlank()) {
+            errors.put("booError", "Não pode ser vazio");
+        }
+
+        JDBIConnection.instance().withExtension(FooRepository.class, dao -> {
+            // Verificar se ocorreram erros no formulário
+            if (errors.isEmpty()) {
+                if (dao.exists(bar)) {
+                    dao.update(foo);
+                } else {
+                    dao.insert(foo);
+                }
+            }
+
+            final var now = LocalDateTime.now();
+            final var count = dao.count();
+            final var foos = dao.selectPage(page, PAGE_SIZE);
+
+            if (errors.isEmpty()) {
+                renderer.render(new FooViewData(foos, now, page, PAGE_SIZE, count));
+            } else {
+                renderer.render(new FooViewData(errors, foo, foos, now, page, PAGE_SIZE, count));
+            }
+
+            return null;
+        });
+
+        response.sendRedirect("/clientes/ClientePage");
     }
 }
