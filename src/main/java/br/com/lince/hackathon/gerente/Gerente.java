0a876467df5f8ad8c88687a05a2f41740b880d31
unknown <Administrador@LIN158.NT-LINCE.SERVER>
2024-07-05 16:26:05 -0300

    alteracoes 2

--- src/main/java/br/com/lince/hackathon/gerente/Gerente.java
+++ src/main/java/br/com/lince/hackathon/gerente/Gerente.java
@@ -1,56 +1,34 @@
 package br.com.lince.hackathon.gerente;
 
-import br.com.lince.hackathon.endereco.Endereco;
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
 public class Gerente {
 
-    private String cpf;
-    private String nome;
-    private int telefone;
-    private String email;
-    private LocalDate dataContratacao;
-    private double percentualComissao;
-    private Endereco endereco;
+    private String nr_cpf;
+    private String nm_gerente;
+    private int nr_telefone;
+    private String ds_email;
+    private String nm_cidade;
+    private String nm_estado;
+    private double pc_comissao;
+    private LocalDate dt_contratacao;
+    private LocalDate dt_nascimento;
 
-    public Gerente(final String cpf, final String nome, final int telefone, final String email, final LocalDate dataContratacao, final double percentualComissao, final Endereco endereco) {
-        this.cpf = cpf;
-        this.nome = nome;
-        this.telefone = telefone;
-        this.email = email;
-        this.dataContratacao = dataContratacao;
-        this.percentualComissao = percentualComissao;
-        this.endereco = new Endereco(endereco.getBairro(), endereco.getCep(), endereco.getCidade(), endereco.getEstado(), endereco.getNumeroResidencia(), endereco.getRua());
-    }
+    public Gerente(){}
 
-    public String getCpf() {
-        return cpf;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public int getTelefone() {
-        return telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public LocalDate getDataContratacao() {
-        return dataContratacao;
-    }
-
-    public double getPercentualComissao() {
-        return percentualComissao;
-    }
-
-    public Endereco getEndereco() {
-        return endereco;
+    public Gerente(String nr_cpf, String nm_gerente, int nr_telefone, String ds_email, String nm_cidade, String nm_estado, double pc_comissao, LocalDate dt_contratacao, LocalDate dt_nascimento) {
+        this.nr_cpf = nr_cpf;
+        this.nm_gerente = nm_gerente;
+        this.nr_telefone = nr_telefone;
+        this.ds_email = ds_email;
+        this.nm_cidade = nm_cidade;
+        this.nm_estado = nm_estado;
+        this.pc_comissao = pc_comissao;
+        this.dt_contratacao = dt_contratacao;
+        this.dt_nascimento = dt_nascimento;
     }
 
     @Override
@@ -58,24 +36,98 @@ public class Gerente {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Gerente gerente = (Gerente) o;
-        return Objects.equals(cpf, gerente.cpf);
+        return Objects.equals(nr_cpf, gerente.nr_cpf);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hashCode(cpf);
+        return Objects.hashCode(nr_cpf);
     }
 
     @Override
     public String toString() {
         return "Gerente{" +
-                "cpf='" + cpf + '\'' +
-                ", nome='" + nome + '\'' +
-                ", telefone=" + telefone +
-                ", email='" + email + '\'' +
-                ", dataContratacao=" + dataContratacao +
-                ", percentualComissao=" + percentualComissao +
-                ", endereco=" + endereco +
+                "nr_cpf='" + nr_cpf + '\'' +
+                ", nm_gerente='" + nm_gerente + '\'' +
+                ", nr_telefone=" + nr_telefone +
+                ", ds_email='" + ds_email + '\'' +
+                ", nm_cidade='" + nm_cidade + '\'' +
+                ", nm_estado='" + nm_estado + '\'' +
+                ", pc_comissao=" + pc_comissao +
+                ", dt_contratacao=" + dt_contratacao +
+                ", dt_nascimento=" + dt_nascimento +
                 '}';
+    }
+
+    public void setNr_cpf(String nr_cpf) {
+        this.nr_cpf = nr_cpf;
+    }
+
+    public void setNm_gerente(String nm_gerente) {
+        this.nm_gerente = nm_gerente;
+    }
+
+    public void setNr_telefone(int nr_telefone) {
+        this.nr_telefone = nr_telefone;
+    }
+
+    public void setDs_email(String ds_email) {
+        this.ds_email = ds_email;
+    }
+
+    public void setNm_cidade(String nm_cidade) {
+        this.nm_cidade = nm_cidade;
+    }
+
+    public void setNm_estado(String nm_estado) {
+        this.nm_estado = nm_estado;
+    }
+
+    public void setPc_comissao(double pc_comissao) {
+        this.pc_comissao = pc_comissao;
+    }
+
+    public void setDt_contratacao(LocalDate dt_contratacao) {
+        this.dt_contratacao = dt_contratacao;
+    }
+
+    public void setDt_nascimento(LocalDate dt_nascimento) {
+        this.dt_nascimento = dt_nascimento;
+    }
+
+    public String getNr_cpf() {
+        return nr_cpf;
+    }
+
+    public String getNm_gerente() {
+        return nm_gerente;
+    }
+
+    public int getNr_telefone() {
+        return nr_telefone;
+    }
+
+    public String getDs_email() {
+        return ds_email;
+    }
+
+    public String getNm_cidade() {
+        return nm_cidade;
+    }
+
+    public String getNm_estado() {
+        return nm_estado;
+    }
+
+    public double getPc_comissao() {
+        return pc_comissao;
+    }
+
+    public LocalDate getDt_contratacao() {
+        return dt_contratacao;
+    }
+
+    public LocalDate getDt_nascimento() {
+        return dt_nascimento;
     }
 }
--- src/main/java/br/com/lince/hackathon/gerente/GerenteRepository.java
+++ src/main/java/br/com/lince/hackathon/gerente/GerenteRepository.java
@@ -20,49 +20,49 @@ public interface GerenteRepository {
      * @param count número de itens na pagina
      * @return lista de Gerentes considerando uma paginação de {count} itens, na página {page}
      */
-//    @RegisterBeanMapper(Gerente.class)
-//    @UseFreemarkerEngine
-//    @SqlQuery("SELECT * FROM gerente ORDER BY nm_gerente OFFSET (${page} * ${count}) ROWS FETCH NEXT ${count} ROWS ONLY")
-//    List<Gerente> selectPage(@Define("page") int page, @Define("count") int count);
-//
-//    /**
-//     * Counts the number of rows in the gerente table
-//     *
-//     * @return the number of rows
-//     */
-//    @UseFreemarkerEngine
-//    @SqlQuery("SELECT COUNT(*) FROM gerente")
-//    int count();
-//
-//    /**
-//     * Carrega o Gerente com o {bar} recebido
-//     *
-//     * @param cpf valor para selecionar o Gerente correto
-//     * @return Gerente
-//     */
-//    @RegisterBeanMapper(Gerente.class)
-//    @UseFreemarkerEngine
-//    @SqlQuery("SELECT * FROM gerente WHERE nr_cpf = :cpf")
-//    Gerente findByCpf(@Bind("cpf") String cpf);
-//
-//    /**
-//     * Verifica se existe um Gerente com o {bar} informado
-//     *
-//     * @param cpf valor para verificar a existência de um Gerente correto
-//     * @return true caso exista um Gerente com o {bar} informado
-//     */
-//    @UseFreemarkerEngine
-//    @SqlQuery("SELECT IIF(EXISTS(SELECT 1 FROM gerente WHERE nr_cpf = :cpf), 1, 0)")
-//    boolean exists(@Bind("cpf") String cpf);
-//
-//    /**
-//     * Remove um Gerente da base de dados
-//     *
-//     * @param cpf valor para apagar identificar o Gerente a ser apagado
-//     */
-//    @UseFreemarkerEngine
-//    @SqlUpdate("DELETE FROM gerente WHERE nr_cpf = :cpf")
-//    void delete(@Bind("cpf") int cpf);
+    @RegisterBeanMapper(Gerente.class)
+    @UseFreemarkerEngine
+    @SqlQuery("SELECT * FROM gerente ORDER BY nm_gerente OFFSET (${page} * ${count}) ROWS FETCH NEXT ${count} ROWS ONLY")
+    List<Gerente> selectPage(@Define("page") int page, @Define("count") int count);
+
+    /**
+     * Counts the number of rows in the gerente table
+     *
+     * @return the number of rows
+     */
+    @UseFreemarkerEngine
+    @SqlQuery("SELECT COUNT(*) FROM gerente")
+    int count();
+
+    /**
+     * Carrega o Gerente com o {bar} recebido
+     *
+     * @param nr_cpf valor para selecionar o Gerente correto
+     * @return Gerente
+     */
+    @RegisterBeanMapper(Gerente.class)
+    @UseFreemarkerEngine
+    @SqlQuery("SELECT * FROM gerente WHERE nr_cpf = :nr_cpf")
+    Gerente findByCpf(@Bind("nr_cpf") String nr_cpf);
+
+    /**
+     * Verifica se existe um Gerente com o {bar} informado
+     *
+     * @param nr_cpf valor para verificar a existência de um Gerente correto
+     * @return true caso exista um Gerente com o {bar} informado
+     */
+    @UseFreemarkerEngine
+    @SqlQuery("SELECT IIF(EXISTS(SELECT 1 FROM gerente WHERE nr_cpf = :nr_cpf), 1, 0)")
+    boolean exists(@Bind("nr_cpf") String nr_cpf);
+
+    /**
+     * Remove um Gerente da base de dados
+     *
+     * @param nr_cpf valor para apagar identificar o Gerente a ser apagado
+     */
+    @UseFreemarkerEngine
+    @SqlUpdate("DELETE FROM gerente WHERE nr_cpf = :nr_cpf")
+    void delete(@Bind("nr_cpf") int nr_cpf);
 
     /**
      * Cadastra um novo Gerente na base de dados.
@@ -70,7 +70,7 @@ public interface GerenteRepository {
      * @param gerente Gerente a ser cadastrado
      */
     @UseFreemarkerEngine
-    @SqlUpdate("INSERT INTO gerente VALUES (:gerente.cpf, :gerente.nome, :gerente.telefone, :gerente.email, :gerente.endereco.cidade, :gerente.endereco.estado, :gerente.percentualComissao, :gerente.dataContratacao)")
+    @SqlUpdate("INSERT INTO gerente VALUES (:gerente.nr_cpf, :gerente.nm_gerente, :gerente.nr_telefone, :gerente.ds_email, :gerente.nm_cidade, :gerente.nm_estado, :gerente.pc_comissao, :gerente.dt_contratacao, :gerente.dt_nascimento)")
     void insert(@BindBean("Gerente") Gerente gerente);
 
     /**
@@ -78,7 +78,7 @@ public interface GerenteRepository {
      *
      * @param gerente Gerente a ser cadastrado
      */
-//    @UseFreemarkerEngine
-//    @SqlUpdate("UPDATE gerente SET nm_gerente = :gerente.nome WHERE nr_cpf = :gerente.cpf")
-//    void update(@BindBean("Gerente") Gerente gerente);
+    @UseFreemarkerEngine
+    @SqlUpdate("UPDATE gerente SET nm_gerente = :gerente.nm_gerente WHERE nr_cpf = :gerente.nr_cpf")
+    void update(@BindBean("Gerente") Gerente gerente);
 }
