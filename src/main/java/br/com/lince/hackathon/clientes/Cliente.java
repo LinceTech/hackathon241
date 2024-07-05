7a68142d4b1724ff297499d8b43fe8c205e15d3b
unknown <Administrador@LIN157.NT-LINCE.SERVER>
2024-07-05 17:05:45 -0300

    cliente insert

---
+++ src/main/java/br/com/lince/hackathon/clientes/Cliente.java
@@ -0,0 +1,147 @@
+package br.com.lince.hackathon.clientes;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private String telefone;
+
+    public Cliente() {
+    }
+
+    private String email;
+    private String cep;
+    private String cidade;
+    private String estado;
+    private String bairro;
+    private String rua;
+    private int numero;
+
+    public Cliente(int id, String nome, String cpf, LocalDate dataNascimento, String telefone, String email, String cep, String cidade, String estado, String bairro, String rua, int numero) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+    }
+
+    public Cliente(String nome, String cpf, LocalDate dataNascimento, String telefone, String email, String cep, String cidade, String estado, String bairro, String rua, int numero) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.telefone = telefone;
+        this.email = email;
+        this.cep = cep;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+}
