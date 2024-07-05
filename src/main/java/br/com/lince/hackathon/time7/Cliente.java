d116158838e76988936744fb63114ac8ce1f9952
Time7 - LIN156 <@hacka7>
2024-07-05 13:21:47 -0300

    Criado classe de Cliente

--- src/main/java/br/com/lince/hackathon/time7/Cliente.java
+++ src/main/java/br/com/lince/hackathon/time7/Cliente.java
@@ -1,4 +1,72 @@
 package br.com.lince.hackathon.time7;
 
+import br.com.lince.hackathon.foo.Foo;
+
+import java.util.Objects;
+
 public class Cliente {
+    public Cliente() {
+    }
+
+    /**
+     * Construir uma instância de Cliente.
+     *
+     * @param id valor para id
+     * @param nome valor para nome
+     */
+    public Cliente(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    /**
+     * Atributo id
+     */
+    private int id;
+
+    /**
+     * Atributo nome
+     */
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cliente cliente = (Cliente) o;
+        return id == cliente.id && Objects.equals(nome, cliente.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
