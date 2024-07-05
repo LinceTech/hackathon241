2bc80bd2076d897b9ab0c57b01c28606129d01af
Time7 - LIN156 <@hacka7>
2024-07-05 17:51:34 -0300

    Setup pro Servlet Veiculo - Rodrigo

---
+++ src/main/java/br/com/lince/hackathon/time7/VeiculoServlet.java
@@ -0,0 +1,174 @@
+package br.com.lince.hackathon.time7;
+
+import br.com.lince.hackathon.standard.JDBIConnection;
+import br.com.lince.hackathon.standard.TemplateRenderer;
+import com.github.jknack.handlebars.internal.lang3.math.NumberUtils;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+/**
+ * Servlet que responde a todas as ações relacionadas ao gerenciamento de veiculos
+ */
+@WebServlet("/veiculo/*")
+public class VeiculoServlet extends HttpServlet {
+    /*
+     * O número de itens na paginação desta tela
+     */
+    private static final int PAGE_SIZE = 5;
+    private static final int now = NumberUtils.toInt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+    /*
+     * Logger padrão do servlet
+     */
+    private static final Logger logger = Logger.getLogger(VeiculoServlet.class.getName());
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var requestPath = request.getPathInfo() != null ? request.getPathInfo() : "";
+
+        switch (requestPath) {
+            case "":
+            case "/":
+                loadFullPage(request, response);
+                break;
+
+            case "/edit":
+                loadFormEdit(request, response);
+                break;
+
+            case "/delete":
+                delete(request, response);
+                break;
+
+            default:
+                response.getWriter().write("Not found : " + requestPath);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var requestPath = request.getPathInfo() != null ? request.getPathInfo() : "";
+
+        if (requestPath.isBlank()) {
+            loadFullPage(request, response);
+        } else if (requestPath.equals("/upsert")) {
+            insertOrUpdate(request, response);
+        } else {
+            response.getWriter().write("Not found : " + requestPath);
+        }
+    }
+
+    /*
+     * Trata a requisição para retorna a página de veiculos carregada com todos os dados
+     */
+    private void loadFullPage(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var renderer = new TemplateRenderer<VeiculoViewData>("veiculo/page", response);
+        final var page = NumberUtils.toInt(request.getParameter("page"), 0);
+
+        JDBIConnection.instance().withExtension(Time7Repository.class, dao -> {
+            final var now = LocalDateTime.now();
+            final var count = dao.count("Veiculo");
+            final var veiculos = dao.selectPageVeiculo(page, PAGE_SIZE);
+
+            renderer.render(new VeiculoViewData(veiculos, now, page, PAGE_SIZE, count));
+
+            return null;
+        });
+    }
+
+    /*
+     * Trata a requisição para inserir ou atualizar um veiculo, e retorna página atualizada
+     */
+    private void insertOrUpdate(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var renderer = new TemplateRenderer<VeiculoViewData>("veiculo/page", response);
+        final var page = NumberUtils.toInt(request.getParameter("page"), 0);
+
+        final var id              = NumberUtils.toInt(request.getParameter("id"), 0);
+        final var marca           = request.getParameter("marca");
+        final var modelo          = request.getParameter("modelo");
+        final var placa           = request.getParameter("placa");
+        final var cor             = request.getParameter("cor");
+        final var anoFabrica      = NumberUtils.toInt(request.getParameter("anoFabrica"), 0);
+        final float custoDiaria   = NumberUtils.toFloat(request.getParameter("custoDiaria"), 0);
+        final var descricao       = request.getParameter("descricao");
+        final var tipoCombustivel = NumberUtils.toInt(request.getParameter("tipoCombustivel"), 0);
+
+        final var veiculo = new Veiculo(id, marca, modelo, placa, cor, anoFabrica, custoDiaria, descricao, tipoCombustivel);
+        final var errors = new HashMap<String, String>();
+
+        if (marca.isBlank()) {
+            errors.put("marcaError", "Não pode ser vazio");
+        } else if (marca.length() > 30) {
+            errors.put("marcaError", "Não pode ser maior que 30 caracteres");
+        }
+
+        if (modelo.isBlank()) {
+            errors.put("modeloError", "Não pode ser vazio");
+        } else if (modelo.length() > 60) {
+            errors.put("modeloError", "Não pode ser maior que 60 caracteres");
+        }
+
+        if (placa.isBlank()) {
+            errors.put("placaError", "Não pode ser vazio");
+        } else if (placa.length() < 7) {
+            errors.put("placa", "Não pode ser menor que 7 caracteres");
+        }
+
+
+
+        JDBIConnection.instance().withExtension(Time7Repository.class, dao -> {
+            // Verificar se ocorreram erros no formulário
+//            if (errors.isEmpty()) {
+//                if (dao.existsVeiculo(id) && id != 0) {
+//                    dao.updateVeiculo(veiculo);
+//                } else {
+//                    dao.insertVeiculo(veiculo);
+//                }
+//            }
+
+            final var now = LocalDateTime.now();
+            final var count = dao.count("Veiculo");
+            final var veiculos = dao.selectPageVeiculo(page, PAGE_SIZE);
+
+            if (errors.isEmpty()) {
+                renderer.render(new VeiculoViewData(veiculos, now, page, PAGE_SIZE, count));
+            } else {
+                renderer.render(new VeiculoViewData(errors, veiculo, veiculos, now, page, PAGE_SIZE, count));
+            }
+
+            return null;
+        });
+    }
+
+    /*
+     * Trata a requisição para alimentar o formulário de cadastro ou edição de veiculos
+     */
+    private void loadFormEdit(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var renderer = new TemplateRenderer<Veiculo>("veiculo/form", response);
+        final var id = NumberUtils.toInt(request.getParameter("id"), 0);
+
+        JDBIConnection.instance().withExtension(Time7Repository.class, dao -> {
+            renderer.render(dao.findByIdVeiculo(id));
+            return null;
+        });
+    }
+
+    /*
+     * Trata a requisição de exclusão de veiculos
+     */
+    private void delete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        final var id = Integer.parseInt(request.getParameter("id"));
+
+        JDBIConnection.instance().withExtension(Time7Repository.class, dao -> {
+            dao.deleteVeiculo(id);
+            loadFullPage(request, response);
+            return null;
+        });
+    }
+}
---
+++ src/main/java/br/com/lince/hackathon/time7/VeiculoViewData.java
@@ -0,0 +1,118 @@
+package br.com.lince.hackathon.time7;
+
+import br.com.lince.hackathon.foo.Foo;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Classe utilizada para agregar todos os dados necessários para alimentar o template veiculo/page
+ */
+public class VeiculoViewData {
+
+    /**
+     * Construtor utilizado para renderizar a página com a lista de veiculos.
+     *
+     * @param veiculos lista de veiculos
+     * @param dateTime data e hora
+     * @param page     número da pagina
+     * @param pageSize número de itens da página atual
+     * @param count    número total de foos
+     */
+    public VeiculoViewData(
+            List<Veiculo> veiculos,
+            LocalDateTime dateTime,
+            int page,
+            int pageSize,
+            int count
+    ) {
+        this.veiculos = veiculos;
+        this.dateTime = dateTime;
+        this.page = page;
+        this.pageSize = pageSize;
+        this.count = count;
+        this.errors = null;
+        this.veiculo = null;
+    }
+
+    /**
+     * Construtor utilizado para renderizar a página com a lista de veiculos.
+     *
+     * @param errors   mapa contendo os errors que ocorreram no formulário
+     * @param veiculo  item a ser alimentado no formulário de cadastro/edição
+     * @param veiculos lista de veiculos
+     * @param dateTime data e hora
+     * @param page     número da pagina
+     * @param pageSize número de itens da página atual
+     * @param count    número total de veiculos
+     */
+    public VeiculoViewData(
+            HashMap<String, String> errors,
+            Veiculo veiculo, List<Veiculo> veiculos,
+            LocalDateTime dateTime,
+            int page,
+            int pageSize,
+            int count
+    ) {
+        this.veiculos = veiculos;
+        this.dateTime = dateTime;
+        this.page = page;
+        this.pageSize = pageSize;
+        this.count = count;
+        this.errors = errors;
+        this.veiculo = veiculo;
+    }
+
+    private final Veiculo veiculo;
+    private final List<Veiculo> veiculos;
+    private final LocalDateTime dateTime;
+    private final int page;
+    private final int pageSize;
+    private final int count;
+    private final HashMap<String, String> errors;
+
+    public HashMap<String, String> getErrors() {
+        return errors;
+    }
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public List<Veiculo> getVeiculos() {
+        return veiculos;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public int getIndex() {
+        return page;
+    }
+
+    public int getPage() {
+        return page + 1;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public int getTotalPages() {
+        return Double.valueOf(Math.ceil(Integer.valueOf(count).doubleValue() / pageSize)).intValue();
+    }
+
+    public boolean getHasPrevious() {
+        return page > 0;
+    }
+
+    public int getPrevious() {
+        return page - 1;
+    }
+
+    public int getNext() {
+        return getPage() < getTotalPages() ? page + 1 : 0;
+    }
+}
