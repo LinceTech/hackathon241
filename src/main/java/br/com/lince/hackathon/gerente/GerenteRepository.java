f9f57df22eeeffb5faa7d268ea8f0700f9d81ade
unknown <Administrador@LIN148.NT-LINCE.SERVER>
2024-07-06 15:31:07 -0300

    locacao

--- src/main/java/br/com/lince/hackathon/gerente/GerenteRepository.java
+++ src/main/java/br/com/lince/hackathon/gerente/GerenteRepository.java
@@ -1,6 +1,6 @@
 package br.com.lince.hackathon.gerente;
 
-import br.com.lince.hackathon.foo.Foo;
+import br.com.lince.hackathon.gerente.Gerente;
 import org.jdbi.v3.freemarker.UseFreemarkerEngine;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
